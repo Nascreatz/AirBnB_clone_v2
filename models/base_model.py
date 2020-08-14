3921539989fe16cb74da296460eb5248682a286f
daviddlhz <vidda1959@gmail.com>
2020-08-13 22:13:28 -0500

    Fix pep8 in models

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-           #storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -47,8 +46,6 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        #dictionary.update({'__class__':
-        #                  (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
